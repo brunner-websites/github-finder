609a8423a0c238bb25f257ca4e8a44ab00ac8838
sandro <sandro@brunner-websites.com>
2020-03-23 00:35:23 +0100

    adding build folder

---
+++ build/precache-manifest.1f0b361415eb3f736dbd759e60ebee47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cdb672d0a98be02b9c75651fb5c2079",
+    "url": "/index.html"
+  },
+  {
+    "revision": "096ced51498ed28c1c5d",
+    "url": "/static/css/main.bc1a1419.chunk.css"
+  },
+  {
+    "revision": "bb56ff09eb656f898af3",
+    "url": "/static/js/2.cb7297c2.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.cb7297c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "096ced51498ed28c1c5d",
+    "url": "/static/js/main.b1bd27a7.chunk.js"
+  },
+  {
+    "revision": "ce717acb90340db90d40",
+    "url": "/static/js/runtime-main.16fff560.js"
+  }
+]);
